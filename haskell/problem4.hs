0b0f09ff5aab5f570839f177c8a6ff611d1fa87c
Alexey Noskov <alexey.noskov@gmail.com>
2011-01-09 16:37:05 +0300

    Problem 4

---
+++ haskell/problem4.hs
@@ -0,0 +1,9 @@
+isPalindrome :: Show a => a -> Bool
+isPalindrome x = s == reverse s where s = show x
+
+minNum = 100
+maxNum = 999
+
+palindromes = [ x * y | x <- [minNum..maxNum], y <- [x..maxNum], isPalindrome (x*y) ]
+
+max_palindrome = foldl1 max palindromes
