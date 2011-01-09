fcbbf851bfecbd45dbd35b469bcbff5ad4df702d
Alexey Noskov <alexey.noskov@gmail.com>
2011-01-10 00:28:52 +0300

    Problem 57

---
+++ haskell/problem57.hs
@@ -0,0 +1,7 @@
+import Data.Ratio
+
+digitNum x = length (show x)
+
+expansions = (3 % 2) : map (\x -> 1 + 1 / (1 + x)) expansions
+
+count = length (filter (\x -> digitNum (numerator x) > digitNum (denominator x) ) (take 1000 expansions))
