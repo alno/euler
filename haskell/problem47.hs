b85d1db8fdee916a71b1c39db16e5b382a5f7f2d
Alexey Noskov <alexey.noskov@gmail.com>
2011-01-11 15:53:32 +0300

    Optimizations

--- haskell/problem47.hs
+++ haskell/problem47.hs
@@ -1,5 +1,3 @@
--- Brute-force solution, need imporvements
-
 import Data.List
 
 noDivisors []    x                  = True
@@ -9,25 +7,29 @@ noDivisors (h:t) x | h*h > x        = True
 
 -- List of prime numbers
 primes :: [Int]
-primes = 2 : filter (noDivisors primes) [3,5..]
+primes = 2 : 3 : filter (noDivisors primes) [ 6*n+ofs | n <- [1..], ofs <- [-1,1] ]
 
 -- Function to check is the number is prime
-isPrime = noDivisors primes
+isPrime x = noDivisors primes x
+
+divMulti x y | m == 0 = divMulti d y
+             | otherwise = x
+    where (d,m) = divMod x y
 
 primeFactorsApp factors (h:t) x | length factors > 4 = Nothing
-                                | h == x             = Just (h:factors)
                                 | h > x              = Just factors
+                                | h * h >= x         = Just (x:factors)
                                 | otherwise          = let (d,m) = divMod x h in
-                                                            if  m == 0 then primeFactorsApp (h:factors) t d
+                                                            if  m == 0 then primeFactorsApp (h:factors) t (divMulti d h)
                                                                        else primeFactorsApp factors t x
 
 primeFactors = primeFactorsApp [] primes
 
 hasNPrimeFactorsApp fc (h:t) n x | fc > n             = False
-                                 | h == x             = fc + 1 == n
                                  | h > x              = fc == n
+                                 | h * h >= x         = fc + 1 == n
                                  | otherwise          = let (d,m) = divMod x h in
-                                                            if  m == 0 then hasNPrimeFactorsApp (fc+1) t n d
+                                                            if  m == 0 then hasNPrimeFactorsApp (fc+1) t n (divMulti d h)
                                                                        else hasNPrimeFactorsApp fc t n x
 
 hasNPrimeFactors = hasNPrimeFactorsApp 0 primes
