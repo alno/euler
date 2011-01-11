82f157df4c7041053799782fbef527eb5b5dca8a
Alexey Noskov <alexey.noskov@gmail.com>
2011-01-11 17:02:09 +0300

    Problem 28

---
+++ haskell/problem28.hs
@@ -0,0 +1,9 @@
+
+diagNumbers n = [ start + sz, start + sz * 2, start + sz * 3, start + sz * 4 ] where sz = n*2; start = (sz-1)*(sz-1)
+
+diagNumberList = [1]:map diagNumbers [1..]
+
+sumList = scanl1 (+) (map sum diagNumberList)
+
+sumList1001 = sumList !! 500
+
