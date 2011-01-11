7c2d16a96d709a3d022930908a80e9066b91a047
Alexey Noskov <alexey.noskov@gmail.com>
2011-01-11 14:27:39 +0300

    Problem 46

---
+++ haskell/problem46.hs
@@ -0,0 +1,21 @@
+noDivisors []    x                  = True
+noDivisors (h:t) x | h*h > x        = True
+                   | x `mod` h == 0 = False
+                   | otherwise      = noDivisors t x
+
+-- List of prime numbers
+primes :: [Integer]
+primes = 2 : filter (noDivisors primes) [3,5..]
+
+-- Function to check is the number is prime
+isPrime = noDivisors primes
+
+isSumOfPrimeAndSquareFrom i n | ds >= n        = False
+                              | isPrime (n-ds) = True
+                              | otherwise      = isSumOfPrimeAndSquareFrom (i+1) n
+    where ds = 2 * i * i
+
+isSumOfPrimeAndSquare = isSumOfPrimeAndSquareFrom 0
+notSumOfPrimeAndSquare = not . isSumOfPrimeAndSquare
+
+notSumNums = filter notSumOfPrimeAndSquare [1,3..]
