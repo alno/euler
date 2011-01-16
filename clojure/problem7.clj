9a84a645a4facc85e8c2a7a56d2c139c1314504b
Alexey Noskov <alexey.noskov@gmail.com>
2011-01-16 17:41:21 +0300

    Problem 7 in Clojure

---
+++ clojure/problem7.clj
@@ -0,0 +1,9 @@
+(defn noDivisors [seq x] (cond
+                           (empty? seq) true
+                           (> (* (first seq) (first seq)) x) true
+                           (= 0 (mod x (first seq))) false
+                           :else (noDivisors (rest seq) x) ))
+
+(def primes (lazy-cat [2 3] (filter (partial noDivisors primes) (iterate (partial + 2) 5)) ))
+
+(defn prime_10001 [] (nth primes 10000))
