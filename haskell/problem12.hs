b148b82fe51dbc81d88d60ef2cd7a07539792901
Alexey Noskov <alexey.noskov@gmail.com>
2011-01-16 18:33:44 +0300

    Problem 12 in Haskell

--- haskell/problem12.hs
+++ haskell/problem12.hs
@@ -13,13 +13,24 @@ nDivsFrom c x y | m == 0 = nDivsFrom (c+1) d y
                 | otherwise = (x,c)
     where (d,m) = divMod x y
 
-nDivs = nDivsFrom 1
+nDivs = nDivsFrom 0
 
 divisorCountFrom cnt (h:t) x | x == 1    = cnt
                              | h * h > x = cnt * 2
-                             | otherwise = divisorCountFrom (cnt * c) t d where (d,c) = nDivs x h
+                             | otherwise = divisorCountFrom (cnt * (c + 1)) t d where (d,c) = nDivs x h
 
 divisorCount = divisorCountFrom 1 primes
+
+appendn x n l | n == 0 = l
+              | n >  0 = appendn x (n-1) (x:l)
+
+primeFactorsDupApp factors (h:t) x | h > x              = factors
+                                   | h * h > x          = (x:factors)
+                                   | otherwise          = primeFactorsDupApp (appendn h c factors) t d where (d,c) = nDivs x h
+
+primeFactorsDup = primeFactorsDupApp [] primes
+
+minNumberWithDivisorCount n = product (zipWith (^) primes (map pred (primeFactorsDup n)))
 
 triangleNumbers :: [Integer]
 triangleNumbers = scanl1 (+) [1..]
@@ -28,8 +39,3 @@ triangleNumbersWithDivisors = map (\x -> (x, divisorCount x)) triangleNumbers
 
 firstWithMoreThan500Divisors = find (\x -> snd x >= 500 ) triangleNumbersWithDivisors
 
-triangleNumberFrom n = n * (n+1) `div` 2
-
-numbersWithDivisors = map (\x -> (x, divisorCount x)) [5..]
-
-firstWithMoreThan200Divisors = find (\x -> snd x >= 100 ) numbersWithDivisors
