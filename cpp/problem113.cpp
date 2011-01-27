15b5282b6db9e16e1c8598ee48e8af4fe2d8adfd
Alexey Noskov <alexey.noskov@gmail.com>
2011-01-27 20:13:44 +0300

    Fix spaces

--- cpp/problem113.cpp
+++ cpp/problem113.cpp
@@ -6,16 +6,16 @@ const int maxLen = 100;
 
 inline uint64 & countIncr( int len, int start ) {
     static uint64 counts[maxLen*10];
-        
+
     return counts[ (len - 1) * 10 + start ];
 }
 
 inline uint64 countIncrSum( int len, int start ) {
     uint64 sum = 0;
-    
+
     for ( int digit = start; digit <= 9; ++ digit )
         sum += countIncr( len - 1, digit );
-    
+
     return sum;
 }
 
@@ -25,24 +25,24 @@ inline uint64 countNotBouncy( int len ) {
 
 inline uint64 countNotBouncyBelow( int len ) {
     uint64 sum = 0;
-    
+
     for ( int i = 1; i <= len; ++ i )
         sum += countNotBouncy( i );
-    
+
     return sum;
 }
 
 int main() {
     for ( int start = 0; start <= 9; ++ start )
         countIncr( 1, start ) = 10 - start;
-        
+
     for ( int len = 2; len <= 100; ++ len ) {
         for ( int start = 0; start <= 9; ++ start )
             countIncr( len, start ) = countIncrSum( len, start );
     }
-    
+
     std::cout << countNotBouncyBelow( 6 ) << std::endl;
-    std::cout << countNotBouncyBelow( 10 ) << std::endl;    
+    std::cout << countNotBouncyBelow( 10 ) << std::endl;
     std::cout << countNotBouncyBelow( 100 ) << std::endl;
 
     return 0;
