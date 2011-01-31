cec95a3306339c777660886754ed84fadd9d2821
Alexey Noskov <alexey.noskov@gmail.com>
2011-01-31 10:40:25 +0300

    Problem 115

--- haskell/problem115.hs
+++ haskell/problem115.hs
@@ -1,10 +1,10 @@
-
+minLength = 50
         
 countsE :: [Integer]
 countsNE :: [Integer]
 
 cntE n = countsE !! (n-1) + countsNE !! (n-1)
-cntNE n = sum (map (countsE !!) [0..n-3])
+cntNE n = sum (map (countsE !!) [0..n-minLength])
 
 countsE = 1 : 1 : map cntE [2..]
 countsNE = 0 : 0 : map cntNE [2..]
@@ -12,4 +12,4 @@ countsNE = 0 : 0 : map cntNE [2..]
 counts = zipWith (+) countsE countsNE
 
 main :: IO ()
-main = print (counts !! 50)
+main = print (length (takeWhile (<=10^6) counts))
