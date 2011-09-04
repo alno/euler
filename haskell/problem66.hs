46a6797b577535ff45d41e5dd0e4d21e6513144f
Alexey Noskov <alexey.noskov@gmail.com>
2011-09-04 19:20:20 +0400

    Problem 66 started

---
+++ haskell/problem66.hs
@@ -0,0 +1,13 @@
+
+intSqrt = ceiling . sqrt . fromIntegral
+
+isSqr x = s * s == x where s = intSqrt x
+      
+squares = [x*x | x <- [1..]]
+
+
+solution d = head [ intSqrt s | y <- squares, let s = d * y + 1, isSqr s ]
+                                                  
+solutions = [ (d, solution d) | d <- [1..], not (isSqr d) ]
+
+main = print "Not completed"
