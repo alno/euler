e8d12608c3068c964b39c19f678ec1b4ca09b81e
Alexey Noskov <alexey.noskov@gmail.com>
2013-03-16 21:52:11 +0400

    Problem 66

--- haskell/problem66.hs
+++ haskell/problem66.hs
@@ -1,13 +1,27 @@
+import Data.Ratio
+import Data.List
+import Data.Function
 
-intSqrt = ceiling . sqrt . fromIntegral
+sqrtFloor = floor . sqrt . fromIntegral
 
-isSqr x = s * s == x where s = intSqrt x
+sqrtExpansionStep s a0 (m,d,a) = (m',d',a')
+  where m' = d*a - m
+        d' = (s - m' * m') `div` d
+        a' = (a0 + m') `div` d'
 
-squares = [x*x | x <- [1..]]
+sqrtExpansion n = map d $ iterate (sqrtExpansionStep n a) (0,1,a)
+  where a         = sqrtFloor n
+        d (_,_,x) = x
 
+isSqr x = s * s == x where s = sqrtFloor x
 
-solution d = head [ intSqrt s | y <- squares, let s = d * y + 1, isSqr s ]
+convergent (h:t) 1 = h % 1
+convergent (h:t) n = h % 1 + 1 / convergent t (n-1)
 
-solutions = [ (d, solution d) | d <- [1..], not (isSqr d) ]
+convergents n = map (convergent $ sqrtExpansion n) [1..]
 
-main = print "Not completed"
+solution d = head $ [ x | c <- convergents d, let x = numerator c, let y = denominator c, x*x - d*y*y == 1 ]
+solutions = [ (d, solution d) | d <- [1..1000], not (isSqr d) ]
+
+main =
+  print $ fst $ maximumBy (compare `on` snd) solutions
